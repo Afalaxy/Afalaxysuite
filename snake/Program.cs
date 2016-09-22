16e39f987f1c24a3a3b76c20cd19857ce36cb378
IB <89111607060@list.ru>
2016-09-22 15:19:21 +0300

    инкапсуляция. создание конструкторов(функций)

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,20 +10,27 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            point p1 = new point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            point p1 = new point(1, 3, '*');
             p1.Draw();
 
-            point p2 = new point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            point p2 = new point(4, 5, '#');
             p2.Draw();
 
+            point p3 = new point(2, 4, '0');
+            p3.Draw();
+
+            point p4 = new point(3, 8, 'r');
+            p3.Draw();
+
+            List<point> pList = new List<point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+            
 
 
+        
             Console.ReadLine();
 
         }
